e389b3fc55f6c9a844ad4984e25cebcebee065a1
anto-pv <antopv833@gmail.com>
2021-12-09 07:54:18 +0530

    3 and 9 th code done by @varghese-babu

---
+++ exp9/answer.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+
+char state[10][100],states[10]="0123456789";
+
+//char gone_state[10];
+//char gone_ctrl=0;
+
+void closure(char st) {
+	
+	int k=0;
+	int i=st-'0';
+	while(state[i][k]!='#') {
+		
+		k++;
+		}
+		
+	if(state[i][k-2]=='N')
+		return;
+	
+		
+	printf("%c,",state[i][k-2]);
+	closure(state[i][k-2]);
+	}
+
+
+void main() {
+	
+	int number_of_states,number_of_input_symbols;
+	
+	printf("enter number of states::");
+	scanf("%d",&number_of_states);
+	printf("\n");
+	printf("enter number of input symbols::");
+	scanf("%d",&number_of_input_symbols);
+	printf("\n");
+	printf("enter the transitions::\n");
+	for(int i=0;i<number_of_states;i++) {
+		
+		scanf("%s",state[i]);
+		}
+		
+	printf("epsilon closure::\n");
+	for(int i=0;i<number_of_states;i++) {
+		
+		printf("closure of %c = { %c,",states[i],states[i]);
+		
+		int k=0;
+		while(state[i][k]!='#') {
+			
+			k++;
+			}
+		if(state[i][k-2]!='N') {
+		
+			printf("%c,",state[i][k-2]);
+			closure(state[i][k-2]);
+			}
+		
+		
+		printf(" }\n");
+		}
+	}
