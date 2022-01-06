a60df2923e3fe23193981ffce78bc03d497e5c3c
anto-pv <antopv833@gmail.com>
2022-01-06 08:29:45 +0530

    added 7th and 8th

--- exp7/answer.cpp
+++ exp7/answer.cpp
@@ -1,1 +1,73 @@
-//will update soon.......
+#include<bits/stdc++.h>
+#define vt vector
+#define umap unordered_map
+
+int main(){
+    using namespace std;
+
+    string input;
+    vt <string> inp;
+    ifstream fp("input.txt",ios::in);
+    while(getline(fp,input))
+        inp.push_back(input);
+    fp.close();
+    cout<<"Enter the number of unrolls required:\n";
+    int n=2;
+    //this code only work for max of loop is mutiple of loop unrolls
+    //cin>>n;
+    int i=0,t=0,cond=0;
+    string ci="",gi="";
+    for(auto x: inp){
+        for(int k=0; k<x.size();k++){
+            if(x[k]=='='){
+                k++;
+                while(x[k]!=';'){
+                    if(x[k]>='0' && x[k]<'9'){
+                        i=i*pow(10,ci.size())+((int)x[k]-48);
+                        ci+=x[k];
+                    }
+                    k++;
+                }
+            }
+            if(x[k]=='<'){
+                k++;
+                ci="";
+                while(x[k]!=')'){
+                    if(x[k]>='0' && x[k]<'9'){
+                        t=t*pow(10,ci.size())+((int)x[k]-48);
+                        ci+=x[k];
+                    }
+                    k++;
+                }
+            }
+            if(x[k]=='{' || cond==1){
+                cond = 1;
+                k++;
+                while(x[k]!='}' && k<x.size()){
+                    gi+=x[k];
+                    k++;
+                }
+                if(x[k]=='}')
+                    cond=0;
+            }
+        }}
+    cout<<i<<gi;
+
+    ofstream sp("unrolledLoop.txt",ios::out);
+    sp<<"for(int i="<<i<<"; i<"<<t<<"; i+="<<n<<" ){";
+    for(int i=0; i<n; i++){
+        sp<<"\n"<<gi<<"\n\ti++;";
+    }
+    sp<<"\n}";
+    sp.close();
+
+    return 0;
+}
+
+/*
+input.txt
+------------------------
+for(int i= 0; i<10;i++){
+    print("Hello world");
+}
+*/
---
+++ exp8/answer.cpp
@@ -0,0 +1,45 @@
+//commom subexpresion elimination
+#include<bits/stdc++.h>
+#define vt vector
+#define umap unordered_map
+
+int main(){
+    using namespace std;
+
+    string a,b,c,d;
+    vt <string> va,vb,vc,vd;
+    cout<<"Enter the three address code: operator var1 var2 res\n";
+    for(int i=0; i<4;i++){
+        cin>>a>>b>>c>>d;
+        va.push_back(a);
+        vb.push_back(b);
+        vc.push_back(c);
+        vd.push_back(d);
+    }
+    char op;string var1,var2,res,temp;
+    umap <string,string> prod;
+    umap <string,string> trash;
+    for(int i=0; i<va.size(); i++){
+        temp="";
+        if(vb[i].substr(0,1)>="A" && vb[i].substr(0,1)<="Z" && trash.count(vb[i]))
+            vb[i]=trash[vb[i]];
+        if(vc[i].substr(0,1)>="A" && vc[i].substr(0,1)<="Z" && trash.count(vc[i]))
+            vc[i]=trash[vc[i]];
+        temp=temp+va[i]+vb[i]+vc[i];
+        if(!prod.count(temp))
+            prod[temp]=vd[i];
+        else
+            trash[vd[i]]=prod[temp];
+    }
+    for(int i=0; i<va.size(); i++){
+        if(!trash.count(vd[i]))
+            cout<<va[i]<<" "<<vb[i]<<" "<<vc[i]<<" "<<vd[i]<<"\n";
+    }
+    return 0;
+}
+/*
+* a b X1
+* X1 d X2
+* a b X3
++ X2 X3 X4
+*/
